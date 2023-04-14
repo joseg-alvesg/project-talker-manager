2496945f0f2badb3bf387c1b5ba5e6513effec5a
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-14 15:13:35 -0300

    req01: route `/talker`

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const readDataFile = require('./utils/readDataFile');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,15 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  try {
+    const data = await readDataFile();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ messsage: 'Deu algum erro aqui' });
+  }
 });
 
 app.listen(PORT, () => {
