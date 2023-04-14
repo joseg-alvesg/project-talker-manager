800277ee335591cc25c9eeaf12a113043691bcb1
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-14 18:14:50 -0300

    implements: `writeFile` need to change name on index

--- src/utils/readDataFile.js
+++ src/utils/readDataFile.js
@@ -1,13 +1,24 @@
 const fs = require('fs/promises');
-const path = require('path');
 
-const dirPath = path.resolve(__dirname, '../talker.json');
-
-module.exports = async () => {
+const readDataFile = async (path) => {
   try {
-    const data = await fs.readFile(dirPath);
+    const data = await fs.readFile(path);
     return JSON.parse(data);
   } catch (error) {
     console.log(error);
   }
+};
+
+const writeDataFile = async (path, att) => {
+  try {
+    const data = await fs.writeFile(path, JSON.stringify(att));
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  readDataFile,
+  writeDataFile,
 };
