61f3f1f73e8274534c34018db76ee8a4fefd56a0
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-15 09:59:18 -0300

    fix: needed validation

--- src/middlewares/talkValid.js
+++ src/middlewares/talkValid.js
@@ -4,5 +4,8 @@ module.exports = (req, res, next) => {
   if (!talk.watchedAt) {
     return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
   }
+  if (talk.rate === undefined) {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  }
   next();
 };
